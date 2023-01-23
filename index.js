b7c4a8828fb300a7d4adaff499e0c81d79d94d92
Viho Akakpo <viheric@gmail.com>
2023-01-23 16:31:57 +0000

    Project done !

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// your first API endpoint... ------------
+app.get('/api/whoami', function(req, res) {
+  res.json({
+    ipaddress: req.ip.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)[0],
+    language: req.get('Accept-Language'),
+    software: req.get('user-agent')
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
